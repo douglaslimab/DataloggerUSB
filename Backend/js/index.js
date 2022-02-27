04dc39b1c820a6c1d5b2d7c9619dd6e78f88c8a6
douglaslimab <douglaslimab@gmail.com>
2022-02-27 21:16:02 +0000

    node access to pg database

--- Backend/js/index.js
+++ Backend/js/index.js
@@ -1,6 +1,7 @@
 PORT = 8000;
 const express = require('express');
-var cors = require('cors');const pg = require('pg')
+var cors = require('cors');
+const pg = require('pg')
 //const client = require('./client')
 
 app = express();
@@ -14,33 +15,15 @@ const client = new pg.Client({
     port: 5432,
 })
 
-async function create_last_hour(){
-    await client.connect();
-    await client.query('CREATE TABLE last_hour(temp_id BIGSERIAL NOT NULL PRIMARY KEY, temperature VARCHAR(8), time VARCHAR(8))');
-    await client.end();
-
-    console.log("last_hour created!!");
-}
-
-async function read_db(){
+app.get('/:sensor', async (req, res) => {
+    const sensor = req.params.sensor
+    
     await client.connect();
     var read_temperature
-
-    read_temperature = await client.query('SELECT temperature FROM temperature_logger ORDER BY temp_id DESC LIMIT 1')
+    read_temperature = await client.query(`SELECT ${sensor}, time FROM temperature_logger ORDER BY temp_id DESC LIMIT 20`)
     await client.end();
 
-    console.log(read_temperature.rows);
-    return read_temperature.rows[0]
-}
-
-app.get('/', (req, res) => {
-    res.send("this is node home..");
+    res.send(read_temperature.rows)
 })
-
-app.get('/temperature', (req, res) => {
-    console.log(read_db);
-    res.json(read_db);
-})
-
 
 app.listen(PORT, () => console.log(`Server running on  PORT: ${PORT}..`));
